a6b958b61c5f5f41f95a99d001b1d5974d417e0d
Margarita Poole <margaritapoole311@gmail.com>
2022-05-01 22:18:39 -0500

    Added state for Random Image URL & for Meme Data

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,19 +1,46 @@
+import { useState } from 'react'
 import memeButtonImage from '../assets/memeButtonImage 🖼.png'
+import memeData from '../memeData'
 
 const Meme = () => {
+  const [memeInfo, setMemeInfo] = useState({
+    topText: '',
+    bottomText: '',
+    randomImage: 'http://i.imgflip.com/1bij.jpg',
+  })
+  
+  const [allMemeImages, setAllMemeImages] = useState({memeData})
+
+  const randomizedNewImg = () => {
+    const memesArray = allMemeImages.memeData.data.memes
+    // console.log('memesArray', memesArray)
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    // console.log('randomNumber', randomNumber)
+    setMemeInfo((prev) => {
+      return {
+        ...prev,
+        randomImage: memesArray[randomNumber].url,
+      }
+    })
+  }
+
+
   return (
     <main className='memeContainer'>
-      <form className='form'>
+      <div className='form'>
         <input type='text' placeholder='Top text' className='formInput'></input>
         <input
           type='text'
           placeholder='Bottom text'
           className='formInput'
         ></input>
-      </form>
-      <button type='button' className='memeButton'>
-        <img src={memeButtonImage} alt='button' />
+      </div>
+      <button type='button' className='memeButton' onClick={randomizedNewImg}>
+        <img src={memeButtonImage} alt='button' width='' height='' />
       </button>
+      <div className='newMeme'>
+        <img src={memeInfo.randomImage} alt='new meme' />
+      </div>
     </main>
   )
 }
