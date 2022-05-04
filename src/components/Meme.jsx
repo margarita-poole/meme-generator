4d2a5d43f99ab631caf95603092a365f1afc547a
Margarita Poole <margaritapoole311@gmail.com>
2022-05-03 19:23:00 -0500

    Added state for input functionality for meme text

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -8,8 +8,8 @@ const Meme = () => {
     bottomText: '',
     randomImage: 'http://i.imgflip.com/1bij.jpg',
   })
-  
-  const [allMemeImages, setAllMemeImages] = useState({memeData})
+
+  const [allMemeImages, setAllMemeImages] = useState({ memeData })
 
   const randomizedNewImg = () => {
     const memesArray = allMemeImages.memeData.data.memes
@@ -24,22 +24,52 @@ const Meme = () => {
     })
   }
 
+  function handleChange(event) {
+    const { name, value } = event.target
+    setMemeInfo((prevMemeInfo) => {
+      return {
+        ...prevMemeInfo,
+        [name]: value,
+      }
+    })
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+  }
 
   return (
     <main className='memeContainer'>
-      <div className='form'>
-        <input type='text' placeholder='Top text' className='formInput'></input>
+      <form className='form' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Top text'
+          className='formInput'
+          name='topText'
+          onChange={handleChange}
+          value={memeInfo.topText}
+        />
         <input
           type='text'
           placeholder='Bottom text'
           className='formInput'
+          name='bottomText'
+          onChange={handleChange}
+          value={memeInfo.bottomText}
         ></input>
-      </div>
-      <button type='button' className='memeButton' onClick={randomizedNewImg}>
+      </form>
+      <button
+        type='submit'
+        className='memeButton'
+        onClick={randomizedNewImg}
+        onSubmit={handleSubmit}
+      >
         <img src={memeButtonImage} alt='button' width='' height='' />
       </button>
-      <div className='newMeme'>
-        <img src={memeInfo.randomImage} alt='new meme' />
+      <div className='meme'>
+        <img src={memeInfo.randomImage} alt='new meme' className='memeImage' />
+        <h2 className='memeText top'>{memeInfo.topText}</h2>
+        <h2 className='memeText bottom'>{memeInfo.bottomText}</h2>
       </div>
     </main>
   )
